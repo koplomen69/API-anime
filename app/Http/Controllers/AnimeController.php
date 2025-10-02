b704332f7418cdfaf10912a0747301a6e40e10e5
shaquille <budum.way@gmail.com>
2025-10-02 12:12:43 +0700

    update

--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\View\View;
-use Illuminate\Support\Str; // Tambah ini kalau butuh Str di controller (tapi sebenarnya optional)
-
 class AnimeController extends Controller
 {
     public function index(): View
